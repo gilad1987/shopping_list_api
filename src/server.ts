e3ad472fbe35cf7482d219446f9cee095d02867a
Gilad <gilad@gilads-mbp.mynet>
2018-07-01 01:14:13 +0300

    .

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import {createServer, Server} from 'http';
 import * as express from 'express';
 import * as socketIo from 'socket.io';
 
-import {Message} from './model';
+var jsonfile = require('jsonfile');
 
 export class ShoppingListServer {
     public static readonly PORT: number = 8080;
@@ -42,45 +42,68 @@ export class ShoppingListServer {
             console.log('Running server on port %s', this.port);
         });
 
+        const file = 'src/db.json';
+
         this.io.on('connect', (socket: any) => {
             console.log('Connected client on port %s.', this.port);
             socket.on('message', (message: any) => {
 
-                if (message.action === 'ADD') {
-                    const exists = this.productList.filter((p) => (p.index === message.product.index));
-                    if (exists.length) {
-                        const index = this.productList.indexOf(exists[0]);
-                        this.productList.splice(index, 1, message.product);
-                    } else {
-                        this.productList.push(message.product);
-                    }
-                }
+                jsonfile.readFile(file, (err: any, db: any) => {
 
-                if (message.action === 'DELETE') {
-                    const exists = this.productList.filter((p) => (p.index === message.product.index));
-                    if (exists.length) {
-                        const index = this.productList.indexOf(exists[0]);
-                        this.productList.splice(index, 1);
+                    if (message.action === 'JOIN') {
+                        message.user.socketId = socket.id;
+                        db.users.push(message.user);
                     }
-                }
 
-                if (message.action === 'CHECK') {
-                    const exists = this.productList.filter((p) => (p.index === message.product.index));
-                    if (exists.length) {
-                        const index = this.productList.indexOf(exists[0]);
-                        this.productList.splice(index, 1, message.product);
+                    if (message.action === 'ADD') {
+                        const exists = db.products.filter((p: any) => (p.index === message.product.index));
+                        if (exists.length) {
+                            const index = db.products.indexOf(exists[0]);
+                            db.products.splice(index, 1, message.product);
+                        } else {
+                            db.products.push(message.product);
+                        }
                     }
-                }
 
-                this.io.emit('message', {
-                    action: message.action,
-                    user: message.user,
-                    list: this.productList,
-                    product: message.product
+                    if (message.action === 'DELETE') {
+                        const exists = db.products.filter((p: any) => (p.index === message.product.index));
+                        if (exists.length) {
+                            const index = db.products.indexOf(exists[0]);
+                            db.products.splice(index, 1);
+                        }
+                    }
+
+                    if (message.action === 'CHECK') {
+                        const exists = db.products.filter((p: any) => (p.index === message.product.index));
+                        if (exists.length) {
+                            const index = db.products.indexOf(exists[0]);
+                            db.products.splice(index, 1, message.product);
+                        }
+                    }
+
+
+                    this.io.emit('message', {
+                        action: message.action,
+                        user: message.user,
+                        db: db
+                    });
+
+                    jsonfile.writeFile(file, db, (err: any) => {});
                 });
+
             });
 
             socket.on('disconnect', () => {
+                jsonfile.readFile(file, (err: any, db: any) => {
+                    const user = db.users.filter((u: any) => (u.socketId === socket.id));
+                    const index = db.users.indexOf(user[0]);
+                    db.users.splice(index, 1);
+                    jsonfile.writeFile(file, db, (err: any) => {});
+                    this.io.emit('message', {
+                        db: db
+                    });
+
+                });
                 console.log('Client disconnected');
             });
         });
